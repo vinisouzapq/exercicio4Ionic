6eb90ec4c7110fab3c58fae14f24d89dafbf2aba
vinisouzapq <mvinicius112015@gmail.com>
2020-06-17 23:27:20 -0300

    Criando page de login, adicionando os imports e fazendo algumas alterações

---
+++ Exercicio4/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule, AngularFireAuth } from '@angular/fire/auth';
+import { IonicStorageModule } from '@ionic/storage';
+
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule,
+    AngularFireAuthModule, 
+    IonicStorageModule.forRoot({
+          name: "__mydb",
+          driverOrder: ['indexeddb', 'sqlite', 'websql']
+      })
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    AngularFireModule,
+    AngularFireAuth,
+
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
